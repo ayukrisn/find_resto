4d6df8e465644b6dc109cc3485048b2d7e975515
ayuukrisn <ayuukrisn@gmail.com>
2025-02-03 00:32:55 +0800

    small fix again

--- lib/screens/error/error_screen.dart
+++ lib/screens/error/error_screen.dart
@@ -10,17 +10,17 @@ class ErrorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center, 
+    return Center(
       child: Padding(
         padding: const EdgeInsets.only(top: 64.0),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset('assets/images/error.png', width: 120,),
             const SizedBox(height: 24,),
             Text("Oops, ada yang salah!", style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),),
             const SizedBox(height: 12,),
-            Text(message),
+            Text(message,  textAlign: TextAlign.center,),
           ],
         ),
       ),
